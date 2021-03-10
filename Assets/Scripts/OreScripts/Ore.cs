4d9bde8d5822a2850ca0c236a26c37c59c601775
Justin Radatti <jdradatti@gmail.com>
2021-03-09 22:11:47 -0800

    ADD: super basic ores

---
+++ Assets/Scripts/OreScripts/Ore.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ore : MonoBehaviour
+{
+    protected string oreName;
+
+    private void Start()
+    {
+        oreName = "this is a generic ore";
+    }
+
+    public void PrintOre()
+    {
+        print(oreName);
+    }
+}
