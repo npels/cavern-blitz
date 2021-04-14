a69670ff3423bfd9278106462e1a5676eede25d7
nicholaspels <mrgekuu@gmail.com>
2021-04-14 16:34:56 -0700

    Added chest object and highlight near chest/crafting

--- Assets/Scripts/InventoryScripts/CraftingStation.cs
+++ Assets/Scripts/InventoryScripts/CraftingStation.cs
@@ -7,12 +7,18 @@ public class CraftingStation : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("Player")) {
             BaseManager.instance.baseUIManager.nearCrafting = true;
+            foreach (Transform child in transform) {
+                child.GetComponent<SpriteRenderer>().color = Color.yellow;
+            }
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision) {
         if (collision.CompareTag("Player")) {
             BaseManager.instance.baseUIManager.nearCrafting = false;
+            foreach (Transform child in transform) {
+                child.GetComponent<SpriteRenderer>().color = Color.white;
+            }
         }
     }
 }
--- Assets/Scripts/InventoryScripts/StockpileChest.cs
+++ Assets/Scripts/InventoryScripts/StockpileChest.cs
@@ -6,12 +6,14 @@ public class StockpileChest : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("Player")) {
             BaseManager.instance.baseUIManager.nearStockpile = true;
+            GetComponent<SpriteRenderer>().color = Color.yellow;
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision) {
         if (collision.CompareTag("Player")) {
             BaseManager.instance.baseUIManager.nearStockpile = false;
+            GetComponent<SpriteRenderer>().color = Color.white;
         }
     }
 }
